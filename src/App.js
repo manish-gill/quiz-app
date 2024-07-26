6813dcbc4661e0ac922b2b325f4d6d426b45a4a9
Manish <manish25gill@gmail.com>
2024-07-26 14:27:44 +0530

    modified:   public/ques.jpg 	new file:   public/ques2.jpg 	deleted:    public/ques3.jpg 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="app" style={{ backgroundImage: 'url("/ques.jpg")' }}>
+      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
         <Header />
         <Switch>
           <Route path="/" exact>
